1bcda1274123c0c7b89f493c5b3441cda87608ef
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-09-16 11:59:30 -0700

    Modify win check methods to be more useful

--- lib/board.rb
+++ lib/board.rb
@@ -27,23 +27,36 @@ class Board
     end
   end
 
-  def col_win?
-    col_one = @board[0][0] == @board[1,0] && @board[1,0] == @board[2,0]
-    col_two = @board[0][1] == @board[1,1] && @board[1,1] == @board[2,1]
-    col_three = @board[0][2] == @board[1,2] && @board[1,2] == @board[2,2]
-    return col_one || col_two || col_three
+  def col_win
+    if col_one = @board[0][0] == @board[1,0] && @board[1,0] == @board[2,0]
+      return 'col_one'
+    elsif col_two = @board[0][1] == @board[1,1] && @board[1,1] == @board[2,1]
+      return 'col_two'
+    elsif col_three = @board[0][2] == @board[1,2] && @board[1,2] == @board[2,2]
+      return 'col_three'
+    end
+    return nil
   end
 
-  def row_win?
-    row_one = @board[0][0] == @board[0,1] && @board[0,1] == @board[0,2]
-    row_two = @board[1][0] == @board[1,1] && @board[1,1] == @board[1,2]
-    row_three = @board[2][0] == @board[2,1] && @board[2,1] == @board[2,2]
-    return row_one || row_two || row_three
+  def row_win
+    if row_one = @board[0][0] == @board[0,1] && @board[0,1] == @board[0,2]
+      return 'row_one'
+    elsif row_two = @board[1][0] == @board[1,1] && @board[1,1] == @board[1,2]
+      return 'row_two'
+    elsif row_three = @board[2][0] == @board[2,1] && @board[2,1] == @board[2,2]
+      return 'row_three'
+    end
+    return nil
   end
 
-  def diag_win?
-    diag_one = @board[0][0] == @board[1][1] && @board[1][1] == @board[2,2]
-    diag_two = @board[0][3] == @board[1][1] && @board[1][1] == @board[2,0]
-    return diag_one || diag_two
+  def diag_win
+    if diag_one = @board[0][0] == @board[1][1] && @board[1][1] == @board[2,2]
+      return 'diag_one'
+    elsif diag_two = @board[0][3] == @board[1][1] && @board[1][1] == @board[2,0]
+      return 'diag_two'
+    end
+    return nil
   end
+
+  
 end
